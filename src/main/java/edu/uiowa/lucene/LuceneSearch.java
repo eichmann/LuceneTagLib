ac75283281f7024e7fac21f4cd400c06057275a1
eichmann <david-eichmann@uiowa.edu>
2012-06-21 21:49:50 +0000

    Added similarity option in search to do a MoreLikeThis query on the queryString rather than a parsed query with operators.  Made this the default as well.

--- src/main/java/edu/uiowa/lucene/LuceneSearch.java
+++ src/main/java/edu/uiowa/lucene/LuceneSearch.java
@@ -8,6 +8,7 @@ package edu.uiowa.lucene;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.StringReader;
 
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.BodyTagSupport;
@@ -19,120 +20,109 @@ import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.queryParser.ParseException;
 import org.apache.lucene.queryParser.QueryParser;
-import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.similar.MoreLikeThis;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.SimpleFSLockFactory;
 
 @SuppressWarnings("serial")
+
 public class LuceneSearch extends BodyTagSupport {
-	TopDocs theHits = null;
-	String label = null;
-	String lucenePath = null;
-	String queryString = null;
-	IndexReader reader = null;
-	IndexSearcher theSearcher = null;
-
+    TopDocs theHits = null;
+    String label = null;
+    String lucenePath = null;
+    String queryString = null;
+    IndexReader reader = null;
+    IndexSearcher theSearcher = null;
+    boolean similarity = true;
+    
 	public static SimpleFSLockFactory _LockFactory;
-	private static final Log log = LogFactory.getLog(LuceneSearch.class);
+    private static final Log log =LogFactory.getLog(LuceneSearch.class);
 
-	public int doStartTag() throws JspException {
-		log.debug("search called: " + queryString);
-		try {
 
-			reader = IndexReader.open(FSDirectory.open(new File(lucenePath), _LockFactory), true);
-			theSearcher = new IndexSearcher(reader);
-			QueryParser theQueryParser = new QueryParser(org.apache.lucene.util.Version.LUCENE_30, label,new StandardAnalyzer(org.apache.lucene.util.Version.LUCENE_30));
-
-			theHits = searchIndex(theSearcher, queryString, 1000,theQueryParser);
-			
-			return EVAL_BODY_INCLUDE;
-		} catch (CorruptIndexException e) {
+    public int doStartTag() throws JspException {
+    	log.debug("search called: " + queryString);
+        try {
+        	reader = IndexReader.open(FSDirectory.open(new File(lucenePath), _LockFactory), true);
+            theSearcher = new IndexSearcher(reader);
+            QueryParser theQueryParser = new QueryParser(org.apache.lucene.util.Version.LUCENE_30, label, new StandardAnalyzer(org.apache.lucene.util.Version.LUCENE_30));
+            Query theQuery = null;
+            
+            if (similarity) {
+            	MoreLikeThis mlt = new MoreLikeThis(reader);
+            	mlt.setMinDocFreq(1);
+            	mlt.setMinTermFreq(1);
+            	mlt.setMaxQueryTerms(100);
+            	theQuery = mlt.like(new StringReader(queryString));
+            } else {
+            	theQuery = theQueryParser.parse(queryString);            	
+            }
+            
+            theHits = theSearcher.search(theQuery, 1000);
+            return EVAL_BODY_INCLUDE;
+        } catch (CorruptIndexException e) {
 			log.error("Corruption Exception", e);
-		} catch (IOException e) {
+        } catch (IOException e) {
 			log.error("IO Exception", e);
-		}
+        }  catch (ParseException e) {
+			log.error("Problem Parseing" + queryString, e);
+        }
 
-		return SKIP_BODY;
-	}
-
-	private static TopDocs searchIndex(IndexSearcher searcher, String queryString, int n, QueryParser theQueryParser) throws IOException {
-		TopDocs myHits = null;
-		boolean retry = true;
-		while (retry) {
-			try {
-				retry = false;
-				Query theQuery = theQueryParser.parse(queryString);
-				myHits = searcher.search(theQuery, n);
-				return myHits;
-			} catch (BooleanQuery.TooManyClauses e) {
-				// Double the number of boolean queries allowed.
-				// The default is in org.apache.lucene.search.BooleanQuery and is 1024.
-				String defaultQueries = Integer.toString(BooleanQuery.getMaxClauseCount());
-				int oldQueries = Integer.parseInt(System.getProperty("org.apache.lucene.maxClauseCount", defaultQueries));
-				int newQueries = oldQueries * 2;
-				log.error("Too many hits for query: " + oldQueries + ".  Increasing to " + newQueries, e);
-				System.setProperty("org.apache.lucene.maxClauseCount", Integer.toString(newQueries));
-				BooleanQuery.setMaxClauseCount(newQueries);
-				retry = true;
-			} catch (ParseException e) {
-				// Double the number of boolean queries allowed.
-				// The default is in org.apache.lucene.search.BooleanQuery and is 1024.
-				String defaultQueries = Integer.toString(BooleanQuery.getMaxClauseCount());
-				int oldQueries = Integer.parseInt(System.getProperty("org.apache.lucene.maxClauseCount", defaultQueries));
-				int newQueries = oldQueries * 2;
-				log.error("Too many hits for query: " + oldQueries + ".  Increasing to " + newQueries, e);
-				System.setProperty("org.apache.lucene.maxClauseCount", Integer.toString(newQueries));
-				BooleanQuery.setMaxClauseCount(newQueries);
-				retry = true;
-			}
-		}
-		return myHits;
-	}
+        return SKIP_BODY;
+    }
 
 	public int doEndTag() throws JspException {
-		try {
-			theSearcher.close();
-			reader.close();
+        try {
+	        theSearcher.close();
+	        reader.close();
 		} catch (IOException e) {
 			log.error("Corruption Exception", e);
 		}
-		return super.doEndTag();
+    	return super.doEndTag();		
+	}
+	
+    private void clearServiceState() {
+        this.theHits = null;
+        this.label = null;
+        reader = null;
+        theSearcher = null;
+        lucenePath = null;
+        queryString = null;
+        similarity = true;
+    }
+
+    public String getLucenePath() {
+        return lucenePath;
+    }
+
+    public void setLucenePath(String lucenePath) {
+        this.lucenePath = lucenePath;
+    }
+
+    public String getQueryString() {
+        return queryString;
+    }
+
+    public void setQueryString(String queryString) {
+        this.queryString = queryString;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+	public boolean isSimilarity() {
+		return similarity;
 	}
 
-	private void clearServiceState() {
-		this.theHits = null;
-		this.label = null;
-		reader = null;
-		theSearcher = null;
-		lucenePath = null;
-		queryString = null;
-	}
-
-	public String getLucenePath() {
-		return lucenePath;
-	}
-
-	public void setLucenePath(String lucenePath) {
-		this.lucenePath = lucenePath;
-	}
-
-	public String getQueryString() {
-		return queryString;
-	}
-
-	public void setQueryString(String queryString) {
-		this.queryString = queryString;
-	}
-
-	public String getLabel() {
-		return label;
-	}
-
-	public void setLabel(String label) {
-		this.label = label;
+	public void setSimilarity(boolean similarity) {
+		this.similarity = similarity;
 	}
 
 }
