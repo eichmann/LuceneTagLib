cf99947d52813c0d66a8cd1bd75387594003402b
eichmann <david-eichmann@uiowa.edu>
2018-06-01 18:44:18 +0000

    Minor reformatting of source.

--- src/main/java/edu/uiowa/lucene/LuceneCount.java
+++ src/main/java/edu/uiowa/lucene/LuceneCount.java
@@ -10,35 +10,33 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.lucene.index.CorruptIndexException;
 
-
 @SuppressWarnings("serial")
 
 public class LuceneCount extends BodyTagSupport {
     LuceneSearch theSearch = null;
-    private static final Log log =LogFactory.getLog(LuceneCount.class);
+    private static final Log log = LogFactory.getLog(LuceneCount.class);
 
-	public int doStartTag() throws JspTagException {
-	    theSearch = (LuceneSearch)findAncestorWithClass(this, LuceneSearch.class);
-		
-		if (theSearch == null) {
-			throw new JspTagException("Lucene Hit tag not nesting in Search instance");
-		}
-		
-        try {
-            pageContext.getOut().print(theSearch.theHits.scoreDocs.length);
-        } catch (CorruptIndexException e) {
-            log.error("Corruption Exception", e);
-        } catch (IOException e) {
-        	log.error("IO Exception", e);
-            
-        }
-		
-		return SKIP_BODY;
+    public int doStartTag() throws JspTagException {
+	theSearch = (LuceneSearch) findAncestorWithClass(this, LuceneSearch.class);
+
+	if (theSearch == null) {
+	    throw new JspTagException("Lucene Hit tag not nesting in Search instance");
 	}
-	
-	public int doEndTag() throws JspException {
-	    return super.doEndTag();        
+
+	try {
+	    pageContext.getOut().print(theSearch.theHits.scoreDocs.length);
+	} catch (CorruptIndexException e) {
+	    log.error("Corruption Exception", e);
+	} catch (IOException e) {
+	    log.error("IO Exception", e);
+
 	}
-	
+
+	return SKIP_BODY;
+    }
+
+    public int doEndTag() throws JspException {
+	return super.doEndTag();
+    }
 
 }
