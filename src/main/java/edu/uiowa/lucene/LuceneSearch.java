20d062c31e1ee998cf94d38eb65a78eeb1615fcf
eichmann <david-eichmann@uiowa.edu>
2021-10-20 19:09:09 +0000

    IndexSearcher instances are now managed by SearcherManager instances (one / Lucene index). This keeps the file caches warm and should cut down on Tomcat container hangs.

--- src/main/java/edu/uiowa/lucene/LuceneSearch.java
+++ src/main/java/edu/uiowa/lucene/LuceneSearch.java
@@ -3,6 +3,7 @@ package edu.uiowa.lucene;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -21,7 +22,6 @@ import org.apache.lucene.facet.search.FacetResult;
 import org.apache.lucene.facet.search.FacetsCollector;
 import org.apache.lucene.facet.taxonomy.CategoryPath;
 import org.apache.lucene.index.CorruptIndexException;
-import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.queries.mlt.MoreLikeThis;
@@ -29,6 +29,7 @@ import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.queryparser.classic.QueryParser.Operator;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
+import org.apache.lucene.search.SearcherManager;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.FSDirectory;
@@ -40,260 +41,279 @@ import edu.uiowa.lucene.biomedical.biomedicalParseFlex;
 import edu.uiowa.lucene.booleanSearch.*;
 import edu.uiowa.lucene.conceptSearch.*;
 import edu.uiowa.lucene.ld4lSearch.LD4LAnalyzer;
-import edu.uiowa.lucene.ld4lSearch.LD4LParser;
 
 @SuppressWarnings("serial")
 
 public class LuceneSearch extends BodyTagSupport {
-    LuceneTaxonomy theTaxonomy = null;
-    FacetsCollector facetCollector = null;
-    List<FacetResult> facetResults = null;
-    
-    TopDocs theHits = null;
-    String label = null;
-    String lucenePath = null;
-    String queryString = null;
-    IndexReader reader = null;
-    IndexSearcher theSearcher = null;
-    String queryParserName = null;
-    boolean useConjunctionByDefault = false;
-    boolean useDateHack = false;
-    boolean useStemming = false;
-    boolean useExactMatch = false;
-    Operator defaultOperator = Operator.AND;
+	public static SimpleFSLockFactory _LockFactory;
+	private static final Log log = LogFactory.getLog(LuceneSearch.class);
+	private static Pattern datePattern = Pattern.compile("([0-9]{4})-([0-9]{4})");
+	@SuppressWarnings("deprecation")
+	static CharArraySet stopWordSet = (new StopAnalyzer(org.apache.lucene.util.Version.LUCENE_30)).getStopwordSet();
 
-    public static SimpleFSLockFactory _LockFactory;
-    private static final Log log = LogFactory.getLog(LuceneSearch.class);
-    private static Pattern datePattern = Pattern.compile("([0-9]{4})-([0-9]{4})");
-    @SuppressWarnings("deprecation")
-    static CharArraySet stopWordSet = (new StopAnalyzer(org.apache.lucene.util.Version.LUCENE_30)).getStopwordSet();
+	static Hashtable<String, SearcherManager> managerHash = new Hashtable<String, SearcherManager>();
+	
+	LuceneTaxonomy theTaxonomy = null;
+	FacetsCollector facetCollector = null;
+	List<FacetResult> facetResults = null;
 
-    public static void main(String[] args) {
-	String originalQuery = "Robert Burton, Robert 1925-1984";
-	log.info("originalQuery:" + originalQuery);
-	log.info("asConjunctiveQuery: " + asConjunctiveQuery(originalQuery));
-	log.info("asConjunctiveQuery w/ date hack: " + asConjunctiveQuery(originalQuery, true));
-    }
+	TopDocs theHits = null;
+	String label = null;
+	String lucenePath = null;
+	String queryString = null;
+	IndexReader reader = null;
+	IndexSearcher theSearcher = null;
+	String queryParserName = null;
+	boolean useConjunctionByDefault = false;
+	boolean useDateHack = false;
+	boolean useStemming = false;
+	boolean useExactMatch = false;
+	Operator defaultOperator = Operator.AND;
 
-    public static String asConjunctiveQuery(String originalQuery) {
-	return asConjunctiveQuery(originalQuery, false);
-    }
-
-    public static String asConjunctiveQuery(String originalQuery, boolean useDateHack) {
-	StringBuffer buffer = new StringBuffer();
-
-	for (String term : originalQuery.split("[, ]+")) {
-	    if (stopWordSet.contains(term.toLowerCase())) {
-		log.info("skipping stop word: " + term);
-		continue;
-	    }
-	    if (buffer.length() > 0)
-		buffer.append(" & ");
-	    if (useDateHack) {
-		Matcher dateMatcher = datePattern.matcher(term);
-		if (dateMatcher.matches()) {
-		    buffer.append(dateMatcher.group(1) + " & " + dateMatcher.group(2));
-		} else
-		    buffer.append(term);
-	    } else {
-		buffer.append(term);
-	    }
+	public static void main(String[] args) {
+		String originalQuery = "Robert Burton, Robert 1925-1984";
+		log.info("originalQuery:" + originalQuery);
+		log.info("asConjunctiveQuery: " + asConjunctiveQuery(originalQuery));
+		log.info("asConjunctiveQuery w/ date hack: " + asConjunctiveQuery(originalQuery, true));
 	}
 
-	return buffer.toString().trim();
-    }
-
-    public int doStartTag() throws JspException {
-	log.info("search called: " + queryString);
-	
-	theTaxonomy = (LuceneTaxonomy) findAncestorWithClass(this, LuceneTaxonomy.class);
-
-	if (useConjunctionByDefault) {
-	    queryString = asConjunctiveQuery(queryString, useDateHack);
-	    log.info("rewriting query to: " + queryString);
+	public static String asConjunctiveQuery(String originalQuery) {
+		return asConjunctiveQuery(originalQuery, false);
 	}
-	log.info("queryParserName: " + queryParserName);
-	
-	try {
-	    reader = DirectoryReader.open(FSDirectory.open(new File(lucenePath)));
-	    theSearcher = new IndexSearcher(reader);
-	    Query theQuery = null;
 
-	    if (theTaxonomy != null) {
-		theTaxonomy.fsp = new FacetSearchParams(theTaxonomy.facetRequests);
-		facetCollector = FacetsCollector.create(theTaxonomy.fsp, theSearcher.getIndexReader(), theTaxonomy.taxoReader);
-		log.trace("taxonomy: " + theTaxonomy);
-		log.trace("facet collector: " + facetCollector);
-	    }
+	public static String asConjunctiveQuery(String originalQuery, boolean useDateHack) {
+		StringBuffer buffer = new StringBuffer();
 
-	    switch (queryParserName) {
-	    case "concept":
-		// TODO insert magic here to allow for selection of nomenclature
-		theQuery = (Query) (new ConceptParseCup(new conceptParseFlex(new StringReader(queryString)))).parse().value;
-		break;
-	    case "boolean":
-		theQuery = (Query) (new BooleanParseCup(new booleanParseFlex(new StringReader(queryString)))).parse().value;
-		break;
-	    case "biomedical":
-		log.info("calling biomedical...");
-		theQuery = (Query) (new BiomedicalParseCup(new biomedicalParseFlex(new StringReader(queryString+" ")))).parse().value;
-		break;
-	    case "similarity":
-		MoreLikeThis mlt = new MoreLikeThis(reader);
-		mlt.setMinDocFreq(1);
-		mlt.setMinTermFreq(1);
-		mlt.setMaxQueryTerms(100);
-		mlt.setFieldNames(new String[] { label });
-		mlt.setAnalyzer(new StandardAnalyzer(Version.LUCENE_43));
-		theQuery = mlt.like(new StringReader(queryString), "content");
-		break;
-	    case "ld4l":
-		if (useExactMatch) {
-		    log.info("exact match: " + queryString);
-		    theQuery =  new TermQuery(new Term(label,queryString));
-		} else {
-		    org.apache.lucene.queryparser.classic.QueryParser ld4lParser = new QueryParser(org.apache.lucene.util.Version.LUCENE_43, label,
-			    new LD4LAnalyzer(useStemming));
-		    ld4lParser.setDefaultOperator(defaultOperator);
-		    theQuery = ld4lParser.parse(queryString);
+		for (String term : originalQuery.split("[, ]+")) {
+			if (stopWordSet.contains(term.toLowerCase())) {
+				log.info("skipping stop word: " + term);
+				continue;
+			}
+			if (buffer.length() > 0)
+				buffer.append(" & ");
+			if (useDateHack) {
+				Matcher dateMatcher = datePattern.matcher(term);
+				if (dateMatcher.matches()) {
+					buffer.append(dateMatcher.group(1) + " & " + dateMatcher.group(2));
+				} else
+					buffer.append(term);
+			} else {
+				buffer.append(term);
+			}
 		}
-		break;
-	    default:
-		org.apache.lucene.queryparser.classic.QueryParser theQueryParser = new QueryParser(org.apache.lucene.util.Version.LUCENE_43, label,
-			new StandardAnalyzer(org.apache.lucene.util.Version.LUCENE_43));
-		theQuery = theQueryParser.parse(queryString);
-		break;
-	    }
 
-	    log.info("actual query: " + theQuery);
+		return buffer.toString().trim();
+	}
+	
+	private static synchronized IndexSearcher getSearcher(String lucenePath) throws IOException {
+		SearcherManager manager = managerHash.get(lucenePath);
+		if (manager == null) {
+			manager = new SearcherManager(FSDirectory.open(new File(lucenePath)), null);
+			managerHash.put(lucenePath, manager);
+		}
+		
+		return manager.acquire();
+	}
+	
+	private static synchronized void releaseSearcher(String lucenePath, IndexSearcher searcher) throws IOException {
+		managerHash.get(lucenePath).release(searcher);
+	}
 
-	    if (theTaxonomy == null) {
-		theHits = theSearcher.search(theQuery, 1000);
-	    } else {
-		if (theTaxonomy.drillDownFacets != null) {
-		    FacetSearchParams fsp = new FacetSearchParams(theTaxonomy.facetRequests);
-		    DrillDownQuery q2 = new DrillDownQuery(fsp.indexingParams,theQuery);
-		    for (String drillDownFacet : theTaxonomy.drillDownFacets) {
-			log.info("\tadding category path: " + drillDownFacet);
-			q2.add(new CategoryPath(drillDownFacet, '/'));
-		    }
+	public int doStartTag() throws JspException {
+		log.info("search called: " + queryString);
+
+		theTaxonomy = (LuceneTaxonomy) findAncestorWithClass(this, LuceneTaxonomy.class);
+
+		if (useConjunctionByDefault) {
+			queryString = asConjunctiveQuery(queryString, useDateHack);
+			log.info("rewriting query to: " + queryString);
+		}
+		log.info("queryParserName: " + queryParserName);
+
+		try {
+			theSearcher = getSearcher(lucenePath);
+			Query theQuery = null;
+
+			if (theTaxonomy != null) {
+				theTaxonomy.fsp = new FacetSearchParams(theTaxonomy.facetRequests);
+				facetCollector = FacetsCollector.create(theTaxonomy.fsp, theSearcher.getIndexReader(),
+						theTaxonomy.taxoReader);
+				log.trace("taxonomy: " + theTaxonomy);
+				log.trace("facet collector: " + facetCollector);
+			}
+
+			switch (queryParserName) {
+			case "concept":
+				// TODO insert magic here to allow for selection of nomenclature
+				theQuery = (Query) (new ConceptParseCup(new conceptParseFlex(new StringReader(queryString))))
+						.parse().value;
+				break;
+			case "boolean":
+				theQuery = (Query) (new BooleanParseCup(new booleanParseFlex(new StringReader(queryString))))
+						.parse().value;
+				break;
+			case "biomedical":
+				log.info("calling biomedical...");
+				theQuery = (Query) (new BiomedicalParseCup(
+						new biomedicalParseFlex(new StringReader(queryString + " ")))).parse().value;
+				break;
+			case "similarity":
+				MoreLikeThis mlt = new MoreLikeThis(reader);
+				mlt.setMinDocFreq(1);
+				mlt.setMinTermFreq(1);
+				mlt.setMaxQueryTerms(100);
+				mlt.setFieldNames(new String[] { label });
+				mlt.setAnalyzer(new StandardAnalyzer(Version.LUCENE_43));
+				theQuery = mlt.like(new StringReader(queryString), "content");
+				break;
+			case "ld4l":
+				if (useExactMatch) {
+					log.info("exact match: " + queryString);
+					theQuery = new TermQuery(new Term(label, queryString));
+				} else {
+					org.apache.lucene.queryparser.classic.QueryParser ld4lParser = new QueryParser(
+							org.apache.lucene.util.Version.LUCENE_43, label, new LD4LAnalyzer(useStemming));
+					ld4lParser.setDefaultOperator(defaultOperator);
+					theQuery = ld4lParser.parse(queryString);
+				}
+				break;
+			default:
+				org.apache.lucene.queryparser.classic.QueryParser theQueryParser = new QueryParser(
+						org.apache.lucene.util.Version.LUCENE_43, label,
+						new StandardAnalyzer(org.apache.lucene.util.Version.LUCENE_43));
+				theQuery = theQueryParser.parse(queryString);
+				break;
+			}
+
+			log.info("actual query: " + theQuery);
+
+			if (theTaxonomy == null) {
+				theHits = theSearcher.search(theQuery, 1000);
+			} else {
+				if (theTaxonomy.drillDownFacets != null) {
+					FacetSearchParams fsp = new FacetSearchParams(theTaxonomy.facetRequests);
+					DrillDownQuery q2 = new DrillDownQuery(fsp.indexingParams, theQuery);
+					for (String drillDownFacet : theTaxonomy.drillDownFacets) {
+						log.info("\tadding category path: " + drillDownFacet);
+						q2.add(new CategoryPath(drillDownFacet, '/'));
+					}
 //		    q2.add(new CategoryPath("Source/CTSAsearch", '/'));
 //		    q2.add(new CategoryPath("Entity/Person/Professor", '/'));
-		    theQuery = q2;
+					theQuery = q2;
+				}
+				log.info("facet query: " + theQuery);
+				theSearcher.search(theQuery, facetCollector);
+				facetResults = facetCollector.getFacetResults();
+				theHits = theSearcher.search(theQuery, 1000);
+			}
+
+			log.debug(theHits.totalHits);
+
+			return EVAL_BODY_INCLUDE;
+		} catch (CorruptIndexException e) {
+			log.error("Corruption Exception", e);
+		} catch (IOException e) {
+			log.error("IO Exception", e);
+		} catch (Exception e) {
+			log.error("Problem Parsing" + queryString, e);
 		}
-		log.info("facet query: " + theQuery);
-		theSearcher.search(theQuery, facetCollector);
-		facetResults = facetCollector.getFacetResults();
-		theHits = theSearcher.search(theQuery, 1000);		
-	    }
 
-	    log.debug(theHits.totalHits);
-
-	    return EVAL_BODY_INCLUDE;
-	} catch (CorruptIndexException e) {
-	    log.error("Corruption Exception", e);
-	} catch (IOException e) {
-	    log.error("IO Exception", e);
-	} catch (Exception e) {
-	    log.error("Problem Parsing" + queryString, e);
+		return EVAL_BODY_INCLUDE;
 	}
 
-	return EVAL_BODY_INCLUDE;
-    }
-
-    public int doEndTag() throws JspException {
-	try {
-	    reader.close();
-	} catch (IOException e) {
-	    log.error("Corruption Exception", e);
+	public int doEndTag() throws JspException {
+		try {
+			releaseSearcher(lucenePath, theSearcher);
+		} catch (IOException e) {
+			log.error("Corruption Exception", e);
+		}
+		clearServiceState();
+		return super.doEndTag();
 	}
-	clearServiceState();
-	return super.doEndTag();
-    }
 
-    private void clearServiceState() {
-	this.theHits = null;
-	this.label = null;
-	reader = null;
-	theSearcher = null;
-	lucenePath = null;
-	queryString = null;
-    }
+	private void clearServiceState() {
+		this.theHits = null;
+		this.label = null;
+		reader = null;
+		theSearcher = null;
+		lucenePath = null;
+		queryString = null;
+	}
 
-    public String getLucenePath() {
-	return lucenePath;
-    }
+	public String getLucenePath() {
+		return lucenePath;
+	}
 
-    public void setLucenePath(String lucenePath) {
-	this.lucenePath = lucenePath;
-    }
+	public void setLucenePath(String lucenePath) {
+		this.lucenePath = lucenePath;
+	}
 
-    public String getQueryString() {
-	return queryString;
-    }
+	public String getQueryString() {
+		return queryString;
+	}
 
-    public void setQueryString(String queryString) {
-	this.queryString = queryString;
-    }
+	public void setQueryString(String queryString) {
+		this.queryString = queryString;
+	}
 
-    public String getLabel() {
-	return label;
-    }
+	public String getLabel() {
+		return label;
+	}
 
-    public void setLabel(String label) {
-	this.label = label;
-    }
+	public void setLabel(String label) {
+		this.label = label;
+	}
 
-    public String getQueryParserName() {
-	return queryParserName;
-    }
+	public String getQueryParserName() {
+		return queryParserName;
+	}
 
-    public void setQueryParserName(String queryParserName) {
-	this.queryParserName = queryParserName;
-    }
+	public void setQueryParserName(String queryParserName) {
+		this.queryParserName = queryParserName;
+	}
 
-    public boolean getUseConjunctionByDefault() {
-	return useConjunctionByDefault;
-    }
+	public boolean getUseConjunctionByDefault() {
+		return useConjunctionByDefault;
+	}
 
-    public void setUseConjunctionByDefault(boolean useConjunctionByDefault) {
-	this.useConjunctionByDefault = useConjunctionByDefault;
-    }
+	public void setUseConjunctionByDefault(boolean useConjunctionByDefault) {
+		this.useConjunctionByDefault = useConjunctionByDefault;
+	}
 
-    public boolean getUseDateHack() {
-	return useDateHack;
-    }
+	public boolean getUseDateHack() {
+		return useDateHack;
+	}
 
-    public void setUseDateHack(boolean useDateHack) {
-	this.useDateHack = useDateHack;
-    }
+	public void setUseDateHack(boolean useDateHack) {
+		this.useDateHack = useDateHack;
+	}
 
-    public boolean getUseStemming() {
-	return useStemming;
-    }
+	public boolean getUseStemming() {
+		return useStemming;
+	}
 
-    public void setUseStemming(boolean useStemming) {
-	this.useStemming = useStemming;
-    }
+	public void setUseStemming(boolean useStemming) {
+		this.useStemming = useStemming;
+	}
 
-    public boolean getUseExactMatch() {
-	return useExactMatch;
-    }
+	public boolean getUseExactMatch() {
+		return useExactMatch;
+	}
 
-    public void setUseExactMatch(boolean useExactMatch) {
-	this.useExactMatch = useExactMatch;
-    }
-    
-    public String getDefaultOperator() {
-	return defaultOperator.toString();
-    }
-    
-    public void setDefaultOperator(String defaultOperator) {
-	this.defaultOperator = Operator.valueOf(defaultOperator);
-    }
+	public void setUseExactMatch(boolean useExactMatch) {
+		this.useExactMatch = useExactMatch;
+	}
 
-    public List<FacetResult> getFacetResults() {
-	return facetResults;
-    }
+	public String getDefaultOperator() {
+		return defaultOperator.toString();
+	}
+
+	public void setDefaultOperator(String defaultOperator) {
+		this.defaultOperator = Operator.valueOf(defaultOperator);
+	}
+
+	public List<FacetResult> getFacetResults() {
+		return facetResults;
+	}
 
 }
